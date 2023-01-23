4659bbc3ecac2dee8019f764e65b958db9b32e97
nukeTK <tarandeepsingh1809@gmail.com>
2023-01-23 18:05:53 +0530

    Completed

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const article1 = require("./api/skysport");
 const article2 = require("./api/espn");
 
+app.get("/", (req, res) => {
+  res.json("Welcome to my football current affairs News API");
+});
+
 app.get("/footballnews", (req, res) => {
   const articles = article1.concat(article2);
   res.json(articles);
