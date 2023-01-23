542daec04adec8869098fc1d98c29a3a618d8ae8
nukeTK <tarandeepsingh1809@gmail.com>
2023-01-23 16:05:05 +0530

    apiupdated

--- app.js
+++ app.js
@@ -7,21 +7,30 @@ const articles = [];
 
 app.get("/", (req, res) => {
   const response = axios
-    .get("https://www.skysports.com/")
+    .get("https://www.skysports.com/football")
     .then((result) => {
       const html = result.data;
       const $ = Cheerio.load(html);
+
+      $('.news-list__item').each((i,el) => {
+        //const title = $(this).text();
+        //const row = $(el).text().replace(/(\s+)/g, ' ');
+        const url = $(el).find("a").attr('href');
+        const news = $(el).find('p').text();
+       //console.log(url.text())
+       articles.push({
+          news,
+          url
+        }); 
+
+      });
+      res.json(articles)
+      //console.log(articles) 
+      const output = $('.news-list__headline');
+      const findText = output.find('a')
       
-       $('.sdc-site-tile__headline', html).each(() => {
-       /*  const title = $(this).text();
-        console.log(title)
-        const url = $(this).attr("href");
-        articles.push({
-          title,
-          url,
-        }); */
-      }); 
-      res.json($)
+      //console.log(findText.html())
+     
     })
     .catch((error) => () => {
     console.log(error)
